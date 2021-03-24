03af50aa9a215f2451afd6beea7122dd061204fb
Asif2535 <aiqbal517@gmail.com>
2021-03-25 00:12:38 +0600

    practice basic CRUD operations

--- index.js
+++ index.js
@@ -19,34 +19,47 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
     const productCollection = client.db("organicdb").collection("products");
-
+    //for show data
     app.get('/products', (req,res)=>{
         productCollection.find({})
         .toArray((err, documents) =>{
             res.send(documents);
         })
     })
-
+    //for show update product
     app.get('/product/:id', (req,res)=>{
         productCollection.find({_id:ObjectId(req.params.id)})
         .toArray((err, documents)=>{
             res.send(documents[0]);
         })
     })
-    
+    //for add
     app.post("/addProduct",(req , res)=>{
         const product = req.body;
         productCollection.insertOne(product)
         .then(result => {
             console.log('data added successfully');
-            res.send('success');
+            // res.send('success');
+            res.redirect('/')
+
+        })
+    })
+    //for update product
+    app.patch('/update/:id',(req,res)=>{
+        productCollection.updateOne({_id:ObjectId(req.params.id)},
+        {
+            $set:{price:req.body.price, quantity:req.body.quantity}
+        })
+        .then(result => {
+            res.send(result.modifiedCount > 0)
         })
     })
 
+    //for delete
     app.delete('/delete/:id', (req,res)=>{
         productCollection.deleteOne({_id:ObjectId(req.params.id)})
         .then(result=>{
-            console.log(result);
+            res.send(result.deletedCount > 0);
         })
     })
 });
